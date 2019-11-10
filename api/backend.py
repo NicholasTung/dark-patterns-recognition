4daa55cdb7d261bae0fe1845c2e8bfda8bcb3610
st0p47 <nicholastung47@gmail.com>
2019-11-10 03:43:23 -0500

    first flask attempt

--- api/backend.py
+++ api/backend.py
@@ -1,11 +1,27 @@
 from flask import Flask, jsonify, request
 from joblib import load
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
 
-count_vect = CountVectorizer()
 presence_classifier = load("presence_classifier.joblib")
+presence_vect = load("presence_vectorizer.joblib")
 category_classifier = load("category_classifier.joblib")
+category_vect = load("category_vectorizer.joblib")
 
-print(presence_classifier.predict(count_vect.transform(["5 left in stock"])))
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    data = request.get_json().get('tokens')
+
+    output = []
+
+    for token in data:
+        result = presence_classifier.predict(presence_vect.transform([token]))
+        output.append(result)
+
+    message = {'result': output}
+
+    return jsonify(message)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', threaded=True, debug=True)
